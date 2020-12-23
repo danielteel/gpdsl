297bcc5ea2f29de1d09fa8ac924a341132f1e86e
Daniel Teel <daniel.teel.2@us.af.mil>
2020-12-23 11:52:00 -0700

    Fixed indentations

--- Interpreter.js
+++ Interpreter.js
@@ -7,112 +7,112 @@ const {Program} = require('./Program');
 
 
 class Interpreter {
-    constructor(){
-    }
+	constructor(){
+	}
 
-    static funcDef(name, func, returnType, ...params){
-        let builtDef={};
+	static funcDef(name, func, returnType, ...params){
+		let builtDef={};
 
-        builtDef.name=name;
-        builtDef.func=func;
-        builtDef.params=[];
+		builtDef.name=name;
+		builtDef.func=func;
+		builtDef.params=[];
 
-        let type=returnType.toLowerCase().trim();
-        switch (type){
-            case "bool":
-                builtDef.type=IdentityType.BoolFunction;
-                break;
-            case "double":
-                builtDef.type=IdentityType.DoubleFunction;
-                break;
-            case "string":
-                builtDef.type=IdentityType.StringFunction;
-                break;
-            default:
-                throw Error("Invalid func return type: '"+returnType+"', it can be bool, double, or string.");
-        }
+		let type=returnType.toLowerCase().trim();
+		switch (type){
+			case "bool":
+				builtDef.type=IdentityType.BoolFunction;
+				break;
+			case "double":
+				builtDef.type=IdentityType.DoubleFunction;
+				break;
+			case "string":
+				builtDef.type=IdentityType.StringFunction;
+				break;
+			default:
+				throw Error("Invalid func return type: '"+returnType+"', it can be bool, double, or string.");
+		}
 
-        for (let i=0;i<params.length;i++){
-            type=params[i].toLowerCase().trim();
-            switch (type){
-                case "bool":
-                    builtDef.params.push(IdentityType.Bool);
-                    break;
-                case "double":
-                    builtDef.params.push(IdentityType.Double);
-                    break;
-                case "string":
-                    builtDef.params.push(IdentityType.String);
-                    break;
-                default:
-                    throw Error("Invalid func parameter type: '"+type+"', it can be bool, double, or string.");
-            }
-        }
-        
-        return builtDef;
-    }
+		for (let i=0;i<params.length;i++){
+			type=params[i].toLowerCase().trim();
+			switch (type){
+				case "bool":
+					builtDef.params.push(IdentityType.Bool);
+					break;
+				case "double":
+					builtDef.params.push(IdentityType.Double);
+					break;
+				case "string":
+					builtDef.params.push(IdentityType.String);
+					break;
+				default:
+					throw Error("Invalid func parameter type: '"+type+"', it can be bool, double, or string.");
+			}
+		}
+		
+		return builtDef;
+	}
 
-    runCode(code, ...externals){
-        let tokenizer=new Tokenizer(code);
-        let errorRecvd=tokenizer.tokenize();
+	runCode(code, ...externals){
+		let tokenizer=new Tokenizer(code);
+		let errorRecvd=tokenizer.tokenize();
 
-        if (errorRecvd!==null){
-            console.log("Error during tokenization on line: "+errorRecvd.line+" "+errorRecvd.message);
-            return errorRecvd;
-        }
+		if (errorRecvd!==null){
+			console.log("Error during tokenization on line: "+errorRecvd.line+" "+errorRecvd.message);
+			return errorRecvd;
+		}
 
-        const parserExternList=[];
-        const executeExternList=[];
-        if (externals){
-            for (let i=0;i<externals.length;i++){
-                if (externals[i] instanceof StringObj){
-                    parserExternList.push({name: externals[i].name, type: IdentityType.String});
-                    executeExternList.push(externals[i]);
-                }else if (externals[i] instanceof NumberObj){
-                    parserExternList.push({name: externals[i].name, type: IdentityType.Double});
-                    executeExternList.push(externals[i]);
-                }else if (externals[i] instanceof BoolObj){
-                    parserExternList.push({name: externals[i].name, type: IdentityType.Bool});
-                    executeExternList.push(externals[i]);
-                }else{            
-                    parserExternList.push(externals[i]);
-                    executeExternList.push(externals[i].func);
-                }
-            }
-        }
+		const parserExternList=[];
+		const executeExternList=[];
+		if (externals){
+			for (let i=0;i<externals.length;i++){
+				if (externals[i] instanceof StringObj){
+					parserExternList.push({name: externals[i].name, type: IdentityType.String});
+					executeExternList.push(externals[i]);
+				}else if (externals[i] instanceof NumberObj){
+					parserExternList.push({name: externals[i].name, type: IdentityType.Double});
+					executeExternList.push(externals[i]);
+				}else if (externals[i] instanceof BoolObj){
+					parserExternList.push({name: externals[i].name, type: IdentityType.Bool});
+					executeExternList.push(externals[i]);
+				}else{            
+					parserExternList.push(externals[i]);
+					executeExternList.push(externals[i].func);
+				}
+			}
+		}
 
-        let parser=new Parser(tokenizer.tokens);
-        const program=parser.parse(parserExternList);//[ {name: "ret", type: IdentityType.String}, {name: "print", type: IdentityType.BoolFunction, params:[IdentityType.String]} ]);
-        if (!(program instanceof Program)){
-            errorRecvd = program;
-            console.log("Error during parse on line: "+errorRecvd.line+" "+errorRecvd.message);
-            return errorRecvd;
-        }
+		let parser=new Parser(tokenizer.tokens);
+		const program=parser.parse(parserExternList);//[ {name: "ret", type: IdentityType.String}, {name: "print", type: IdentityType.BoolFunction, params:[IdentityType.String]} ]);
+		if (!(program instanceof Program)){
+			errorRecvd = program;
+			console.log("Error during parse on line: "+errorRecvd.line+" "+errorRecvd.message);
+			return errorRecvd;
+		}
 
-        program.printDebugView(true);
+		program.printDebugView(true);
 
-        errorRecvd=program.link();
-        if (errorRecvd){
-            console.log("Error during link: "+errorRecvd.line+" "+errorRecvd.message);
-            return errorRecvd;
-        }
+		errorRecvd=program.link();
+		if (errorRecvd){
+			console.log("Error during link: "+errorRecvd.line+" "+errorRecvd.message);
+			return errorRecvd;
+		}
 
-        program.printDebugView(true);
+		program.printDebugView(true);
 
-        const startTime = new Date().getTime();
-        let exitObject=program.execute(executeExternList);
-        const endTime = new Date().getTime();
+		const startTime = new Date().getTime();
+		let exitObject=program.execute(executeExternList);
+		const endTime = new Date().getTime();
 
-        if (!(exitObject instanceof OpObj)){
-            errorRecvd=exitObject;
-            console.log("Error during execution on line: "+errorRecvd.line+" "+errorRecvd.message);
-            return errorRecvd;
-        }
-        
-        console.log("Exit value:"+exitObject.value);
-        console.log("Execution time: "+(endTime-startTime));
-        return exitObject;
-    }
+		if (!(exitObject instanceof OpObj)){
+			errorRecvd=exitObject;
+			console.log("Error during execution on line: "+errorRecvd.line+" "+errorRecvd.message);
+			return errorRecvd;
+		}
+		
+		console.log("Exit value:"+exitObject.value);
+		console.log("Execution time: "+(endTime-startTime));
+		return exitObject;
+	}
 }
 
 module.exports={Interpreter, StringObj, NumberObj, BoolObj};
--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 let code = `double reverseInteger(double number) { 
-    double newNumber=0;
-    while (number!=0){
-        newNumber=newNumber*10+number%10;
-        if (number>0){
-          number=floor(number/10);
-        }else{
-          number=ceil(number/10);
-        }
-    }
-    return newNumber;
+		double newNumber=0;
+		while (number!=0){
+				newNumber=newNumber*10+number%10;
+				if (number>0){
+					number=floor(number/10);
+				}else{
+					number=ceil(number/10);
+				}
+		}
+		return newNumber;
 }
 
 for (double i=0;i<=1000000;i=i+1){
-   reverseInteger(i);
+	 reverseInteger(i);
 }
 double i;
 print( tostring(?i, null) );
@@ -26,24 +26,24 @@ bool d=0;
 
 double A=1, B=2, C=3, D=4, EL=5;
 exit    a ? A
-      : b ? B
-      : c ? C
-      : d ? D
-      : EL;
+			: b ? B
+			: c ? C
+			: d ? D
+			: EL;
 `;
 
 const {Interpreter, StringObj, NumberObj, BoolObj} = require('./Interpreter');
 
 function print(popFn){
-    let str=popFn();
+		let str=popFn();
 
-    console.log(str.value);
-    return new BoolObj(null, false, false);
+		console.log(str.value);
+		return new BoolObj(null, false, false);
 }
 
 let interpreter=new Interpreter();
 
 interpreter.runCode( 
-                    code,                                                   //The code to run
-                    Interpreter.funcDef("print", print, "bool", "string"),  //importing print (returns a bool, accepts a string)
-                   );
+										code,                                                   //The code to run
+										Interpreter.funcDef("print", print, "bool", "string"),  //importing print (returns a bool, accepts a string)
+									 );
