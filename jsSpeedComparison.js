43bd04802a6e6d55507226a5f1cdcb2ef44c984c
danielteel <74428738+danielteel@users.noreply.github.com>
2021-01-22 14:00:56 -0700

    Delete jsSpeedComparison.js

--- jsSpeedComparison.js
+++
@@ -1,7 +0,0 @@
-function fib(n) {
-    if (n < 2) return n;
-    return fib(n - 1) + fib(n - 2); 
-}
-let startTime=new Date().getTime();
-console.log(fib(40));
-console.log((new Date().getTime())-startTime);
